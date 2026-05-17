be2895d258b6e9ef4ffdebaf2f55bdddd9d3c85f
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-05-17 18:49:40 +0530

    add jwt

---
+++ student-manager/src/main/java/com/badri/student_manager/UserRepository.java
@@ -0,0 +1,10 @@
+package com.badri.student_manager;
+
+import com.badri.student_manager.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findNameByEmail(String email);
+}
