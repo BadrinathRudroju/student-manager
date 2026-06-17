296ef01d5fbf3575cfcbefad1c91483a338c2f33
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-06-17 09:43:37 +0530

    feat: add dto

---
+++ student-manager/src/main/java/com/badri/student_manager/dto/StudentRequest.java
@@ -0,0 +1,18 @@
+package com.badri.student_manager.dto;
+
+import jakarta.validation.constraints.*;
+
+public record StudentRequest(
+        @NotBlank(message = "It should have a name")
+        @Size(min = 2, max = 50, message= "name should be betweeen 2 to 50 characters")
+        String name,
+
+        @NotBlank(message = "email is mandatory")
+        @Email(message = "email must be valid")
+        String email,
+
+        @Min(value = 1, message = "age should be at least 1")
+        @Max(value = 100, message ="age should not exceed 100")
+        String branch
+) {
+}
